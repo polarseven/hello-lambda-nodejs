524826349991c75fd48fb801c5972abb5f5b8725
Matt Warman <warmanmr@gmail.com>
2017-09-15 11:37:22 -0400

    Hello World function.

---
+++ src/main/js/index.js
@@ -0,0 +1,22 @@
+const _ = require('lodash');
+
+isNullOrEmpty = (str) => {
+  return _.isNull(str) || _.isUndefined(str) || _.isEmpty(str)
+}
+
+exports.handler = (event, context, callback) => {
+  console.log("> handler");
+  console.log("- name:" + event.name);
+
+  const defaultText = "Hello World!";
+  var response = {};
+
+  if (isNullOrEmpty(event.name)) {
+    response.text = defaultText;
+  } else {
+    response.text = "Hello " + event.name + "!";
+  }
+
+  console.log("< handler");
+  callback(null, response);
+};
