d1c30ee9bc905c87547357c2a9e0dbf867d58ea9
Matt Warman <warmanmr@gmail.com>
2017-09-15 13:43:54 -0400

    Uppercase function.

---
+++ src/main/js/uppercase.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+
+exports.handler = (event, context, callback) => {
+  console.log("> handler");
+
+  var response = {};
+
+  _.forIn(event, (value, key) => {
+    console.log(`- key:${key}, value:${value}`);
+    if (_.isString(value)) {
+      _.set(response, key, _.toUpper(value));
+    } else {
+      _.set(response, key, value);
+    }
+  })
+
+  console.log("< handler");
+  callback(null, response);
+};
