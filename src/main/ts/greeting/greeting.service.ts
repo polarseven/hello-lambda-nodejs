f8f001bafb6e2c0bae0aac78b6e2ed80c0f14437
Matt Warman <warmanmr@gmail.com>
2017-09-19 11:01:37 -0400

    Added Observable pattern to GreetingService.

--- src/main/ts/greeting/greeting.service.ts
+++ src/main/ts/greeting/greeting.service.ts
@@ -1,4 +1,6 @@
 import * as AWS from 'aws-sdk';
+import * as Rx from 'rxjs/Rx';
+import * as _ from 'lodash';
 
 import { Greeting } from './greeting';
 
@@ -7,7 +9,7 @@ export class GreetingService {
   tableName: string = process.env.TABLE_NAME;
   ddb: AWS.DynamoDB = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
 
-  findById(id: string): void {
+  findById(id: string): Rx.Observable<Greeting>|any {
     console.log(`> findAll`);
 
     const params: any = {
@@ -16,19 +18,25 @@ export class GreetingService {
         'id': { S: `${id}` }
       }
     };
-
     console.log(`- params: ${JSON.stringify(params)}`);
 
-    this.ddb.getItem(params, (err, data) => {
-      if (err) {
-        console.error(err);
-        console.log(`< findAll`);
-      } else {
-        console.log(`- data: ${JSON.stringify(data)}`);
-        console.log(`< findAll`);
-      }
-    });
+    const getItem: any =
+      Rx.Observable.bindNodeCallback(this.ddb.getItem.bind(this.ddb));
 
+    console.log(`< findAll`);
+    return getItem(params)
+      .map((data: any) => {
+        console.log(`- map`);
+        console.log(`- data: ${JSON.stringify(data)}`);
+        if (data.Item) {
+          const greeting: Greeting = new Greeting();
+          greeting.id = _.get(data, 'Item.id.S', '');
+          greeting.text = _.get(data, 'Item.text.S', '');
+          return greeting;
+        } else {
+          return {};
+        }
+      });
   }
 
 }
