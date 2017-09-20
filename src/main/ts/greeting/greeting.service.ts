4d08fb28ea09d9d97b2538874c203a7e8c8e6d2e
Matt Warman <warmanmr@gmail.com>
2017-09-20 06:47:23 -0400

    Initial logic for CreateGreeting.

--- src/main/ts/greeting/greeting.service.ts
+++ src/main/ts/greeting/greeting.service.ts
@@ -1,6 +1,7 @@
 import * as AWS from 'aws-sdk';
 import * as Rx from 'rxjs/Rx';
 import * as _ from 'lodash';
+import * as uuid from 'uuid/v4';
 
 import { Greeting } from './greeting';
 
@@ -36,6 +37,30 @@ export class GreetingService {
         } else {
           return {};
         }
+      });
+  }
+
+  save(greeting: Greeting): Rx.Observable<Greeting>|any {
+    console.log(`> save`);
+
+    const params: any = {
+      TableName: this.tableName,
+      Item: {
+        'id': { S: `${greeting.id || uuid()}` },
+        'text': { S: `${greeting.text}` }
+      }
+    };
+    console.log(`- params: ${JSON.stringify(params)}`);
+
+    const putItem: any =
+      Rx.Observable.bindNodeCallback(this.ddb.putItem.bind(this.ddb));
+
+    console.log(`< save`);
+    return putItem(params)
+      .map((data: any) => {
+        console.log(`- map`);
+        console.log(`- data: ${JSON.stringify(data)}`);
+        return data;
       });
   }
 
